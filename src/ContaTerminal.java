3f9fbbdbe28c141891cd146459c58292684a6bdd
LosSantins <vieirarg1@outlook.com>
2023-05-20 02:27:15 -0300

    Finish

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Ola!");
 
-        System.out.println("OI!")
+
+        // Definindo Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Agencia e Numero da conta pré-definidos!
+        int numero = 1001;
+        String agencia = "234-5";
+
+        System.out.println("Digite Seu Primeiro Nome: ");
+        String nomeCliente = sc.next();
+
+        System.out.println("Digite Seu Ultimo Nome: ");
+        String sobrenomeCliente = sc.next();
+
+        System.out.println("Qual o valor do seu primeiro deposito? ");
+        float saldo = sc.nextFloat();
+
+
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque");
     }
 }
